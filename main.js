4bbb91792bd0be4ee02fa57b36ecf0fe92230b11
Daryl Rogony <darylrogony@gmail.com>
2022-04-24 17:38:42 +0300

    Completed Assignment

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ const FULL_HEART = '♥'
 
 
 
+const hearts = document.querySelectorAll( ".like-glyph");
+
+  function callBack(e) {
+    const heart = e.target;
+    mimicServerCall("url")
+    .then(function() {
+      if ( heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      }else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() => modal.className = "hidden", 3000);
+    });
+  }
+  for (const glyph of hearts) {
+    glyph.addEventListener("click", callBack);
+  }
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
